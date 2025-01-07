40f85cbb9127f05ef8c334e01cfd8c21f0682295
albertina777 <66668664+albertina777@users.noreply.github.com>
2025-01-07 21:33:21 +0530

    Update app.js

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -8,18 +8,31 @@ var greetingRouter = require('./routes/greeting');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Set custom header for all requests 
 
-app.use('/', indexRouter);
-app.use('/greeting', greetingRouter);
+app.use((req, res, next) => { 
+  res.setHeader('X-Custom-Header', 'AlbertinaValue'); 
+  next(); 
+}); 
 
-app.use((req, res) => {
-  res.setHeader('X-Custom-Header', 'AlbertinaValue');
-});
+// Use greetingRouter for requests to /greeting 
+
+app.use('/greeting', greetingRouter); 
+
+app.get('/', (req, res) => { res.send('Hello World'); });
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/greeting', greetingRouter);
+
+// app.use((req, res) => {
+//   res.setHeader('X-Custom-Header', 'AlbertinaValue');
+// });
 
 
 module.exports = app;
