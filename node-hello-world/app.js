de861ff5fa0079c5eaaa67c5d881e19fd6fed254
albertina777 <66668664+albertina777@users.noreply.github.com>
2025-01-07 21:13:12 +0530

    Update app.js

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -17,4 +17,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/greeting', greetingRouter);
 
+app.use((req, res) => {
+  res.setHeader('X-Custom-Header', 'AlbertinaValue');
+});
+
+
 module.exports = app;
